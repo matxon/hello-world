9b585acbceb2a3fbdb40fecb8dcafa653544e4a8
matxon <kmpda@inbox.ru>
2014-10-28 00:45:49 +0500

    adding new files

--- app.js
+++ app.js
@@ -2,59 +2,52 @@
 // 4.9.4
 
 var express = require('express'),
-    path = require('path'),
-    mysqldb = require('./mysqltest'),
-    router = express.Router(),
-    http = require('http');
-
-var options = {
-  host: 'localhost',
-  user: 'matxon',
-  database: 'test',
-  password: ''
-};
+    routes = require('./routes/index'); //express.Router(),
 
 var app = express();
-var testdb = new mysqldb( options );
-
-testdb.connect( function( err ) {
-  if (err) console.log( "/db connect: " + err.stack ); 
-});
 
 // configure
 app.set('port', process.env.PORT || 3000 );
 // express-те турып jade движогын пайдалану ушін керек
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-//app.set('public', __dirname + '/node_modules/bootstrap/dist'); 
 
 // middleware
-//app.use('/', routes);
-app.use(express.static(path.join(__dirname, 'public'))); 
-app.use('/', router);
+app.use(express.static(__dirname + '/public')); 
+app.use('/', routes);
 
-// route
-router.get( '/', function( req, res ) {
-  res.render( "home", { title: "My App with Express and jade." } ); 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-router.get( '/db', function( req, res ) {
+// error handlers
 
-  var query = 'select * from users';
-  testdb.query( query, function( err, data ) {
-    if (err) {
-      res.send( "/db.query error" );
-      return;
-    } 
-    res.render( 'data', {tabl:data} );
-  });
-}); 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
-http.createServer( app ).listen( app.get('port'), function() {
-  console.log( "Express server listening on port " + app.get( 'port' ));
-/*});
-*/
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
-//var server = app.listen(app.get('port'), function() {
-//  console.log('Express server listening on port ' + server.address().port);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
 });
