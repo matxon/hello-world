002965836ce6b9d6ca5fc120578fa0ae2949085a
matxon <kmpda@inbox.ru>
2014-11-06 03:59:22 +0500

    add and update

--- public/js/users.js
+++ public/js/users.js
@@ -2,7 +2,7 @@ $(function() {
   var dialog; 
   var form = $("form");
   var name = $("#name");
-  var ed = $("#autocomplete");
+  var pass = $("#password");
       
   var dialogOptions = {
     autoOpen: false,
@@ -21,11 +21,13 @@ $(function() {
   function addTable() {
     var valid = true;
     
+    var regexp = "\[0-9A-za-z]";
+
     if ( valid ) {
       $( "#charges tbody" ).append( "<tr>" +
                                  "<td>+</td>" +
         "<td>" + name.val() + "</td>" +
-          "<td>" + ed.val() + "</td>" +
+          "<td>" + pass.val() + "</td>" +
           '<td><button type="button" class="btn btn-default btn-xs">' +
           '<span class="glyphicon glyphicon-pencil"></span></button>' +
           '<button type="button" class="btn btn-default btn-xs">' +
@@ -37,8 +39,18 @@ $(function() {
   }
 
   dialog = $("#dialog-form").dialog(dialogOptions);
+
   $("#addcharges").click(function() { dialog.dialog("open") });
 
+  $("#test").click( function() { 
+    var td = $("td");
+    var temp = [];
+    for (var i = 0; i < td.length; i += 4) {
+      temp.push({ id:'', name: td[i+1].innerText, pass: td[i+2].innerText });
+    }
+
+    console.log(temp);
+  });
   dialog.find( "form" ).on( "submit", function( event ) {
     event.preventDefault();
     addTable();
